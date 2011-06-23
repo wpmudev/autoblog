4950e459aec94f0c2160a2a70ad1f7a64f41ea9c
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-06-23 17:42:40 +0000

    quick twitter addon fix
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/autoblog@3674 f53fe46e-8c02-459b-9fdd-93e75771b278

--- autoblogincludes/plugins/twitter.addon.php
+++ autoblogincludes/plugins/twitter.addon.php
@@ -71,7 +71,7 @@ class A_twitter_addon {
 
 	// Function to generate twitter based links from http://www.snipe.net/2009/09/php-twitter-clickable-links/
 	function twitterify($ret) {
-		$ret = preg_replace("^(\w+):(.*)^", "\\2", $ret);
+		$ret = preg_replace("^(\w+): (.*)^", "\\2", $ret);
 	  	$ret = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t< ]*)#", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $ret);
 	  	$ret = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r< ]*)#", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>", $ret);
 	  	$ret = preg_replace("/@(\w+)/", "<a href=\"http://www.twitter.com/\\1\" target=\"_blank\">@\\1</a>", $ret);
@@ -80,7 +80,7 @@ class A_twitter_addon {
 	}
 
 	function strip_account($ret) {
-		$ret = preg_replace("^(\w+):(.*)^", "\\2", $ret);
+		$ret = preg_replace("^(\w+): (.*)^", "\\2", $ret);
 		return $ret;
 	}
 
