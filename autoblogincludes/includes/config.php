f7e1ab514888e7c324cde9ba2bce8f9aacc8721d
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-07-02 18:56:59 +0000

    updated config file option descriptions
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/autoblog@6514 f53fe46e-8c02-459b-9fdd-93e75771b278

--- autoblogincludes/includes/config.php
+++ autoblogincludes/includes/config.php
@@ -4,17 +4,14 @@
 // Uses a global table so that all entries can be managed by the network admin as well
 if(!defined('AUTOBLOG_GLOBAL')) define( 'AUTOBLOG_GLOBAL', true );
 
-// Processing will stop after 3 seconds (default) so as not to overload your server
+// Processing will stop after 6 seconds (default) so as not to overload your server
 if(!defined('AUTOBLOG_PROCESSING_TIMELIMIT')) define( 'AUTOBLOG_PROCESSING_TIMELIMIT', 6);
 
-// Processing will take place every 10 minutes (options are 10 or 5).
+// Processing will take place every minute
 if(!defined('AUTOBLOG_PROCESSING_CHECKLIMIT')) define( 'AUTOBLOG_PROCESSING_CHECKLIMIT', 1);
 
 // In a multisite install will attempt to process feeds for all sites rather than just local ones
 if(!defined('AUTOBLOG_FORCE_PROCESS_ALL')) define( 'AUTOBLOG_FORCE_PROCESS_ALL', false);
-
-// Will check for feeds to process on every page load rather than using the limit defined above
-if(!defined('AUTOBLOG_PROCESS_EVERY_PAGE_LOAD')) define( 'AUTOBLOG_PROCESS_EVERY_PAGE_LOAD', false);
 
 // Uses a different, more processing intensive, method of adding tags to a post for sites that have tag based issues
 if(!defined('AUTOBLOG_HANDLE_FAKE_TAGS')) define( 'AUTOBLOG_HANDLE_FAKE_TAGS', true);
