c57731570ad26810eed49d9101ec526011ddb773
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-07-16 16:49:16 +0000

    added canonical header add-on started original author add-on
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/autoblog@9737 f53fe46e-8c02-459b-9fdd-93e75771b278

--- autoblogincludes/addons/addcanonicalheader.php
+++ autoblogincludes/addons/addcanonicalheader.php
@@ -1,1 +1,52 @@
 <?php
+/*
+Addon Name: Canonical link in header
+Description: Adds a rel='canonical' link in a posts header to point to the original post
+Author: Barry (Incsub)
+Author URI: http://premium.wpmudev.org
+*/
+
+class A_canonicalheader {
+
+	var $build = 1;
+
+	function __construct() {
+
+		add_action( 'wp_head', array( &$this, 'output_canonical_header' ), 9 );
+
+	}
+
+	function A_canonicalheader() {
+		$this->__construct();
+	}
+
+	function output_canonical_header() {
+
+		global $post;
+
+		// Check if we are on a single item page
+		if( !is_singular() ) {
+			return;
+		}
+
+		if( !empty($post) ) {
+			// We have a post - so we can look for the information we want
+			$original_link = get_post_meta( $post->ID, 'original_source', true );
+
+			if( !empty($original_link) ) {
+				// We have a link - so output it as the canonical
+				echo "<link rel='canonical' href='" . esc_attr($original_link) . "' />\n";
+
+				// Check and remove any standard actions so we don't duplicate.
+				if( has_action( 'wp_head', 'rel_canonical' ) ) {
+					remove_action( 'wp_head', 'rel_canonical' );
+				}
+			}
+
+		}
+
+	}
+
+}
+
+$acanonicalheader = new A_canonicalheader();
---
+++ autoblogincludes/addons/displayoriginalauthor.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Addon Name: Replace Author Information
+Description: Replaces the author details shown on a post with the imported sites ones
+Author: Barry (Incsub)
+Author URI: http://premium.wpmudev.org
+*/
+
+class A_importedauthor {
+
+	var $build = 1;
+
+	function __construct() {
+
+
+
+	}
+
+	function A_importedauthor() {
+		$this->__construct();
+	}
+
+
+
+}
+
+$aimportedauthor = new A_importedauthor();
