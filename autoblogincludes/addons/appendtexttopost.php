49277b05823641312865dd3b699a664dc2cd3155
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-07-16 20:32:08 +0000

    started add-on to enable specific text to be placed beneath each post
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/autoblog@9740 f53fe46e-8c02-459b-9fdd-93e75771b278

---
+++ autoblogincludes/addons/appendtexttopost.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Addon Name: Append text to post
+Description: Allows some text to be appended to each post with variable placeholders
+Author: Barry (Incsub)
+Author URI: http://premium.wpmudev.org
+*/
+
+class A_appendtexttopost {
+
+	var $build = 1;
+
+	function __construct() {
+
+
+
+	}
+
+	function A_appendtexttopost() {
+		$this->__construct();
+	}
+
+
+
+
+}
+
+$aappendtexttopost = new A_appendtexttopost();
