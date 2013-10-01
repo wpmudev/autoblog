43eeaf7aaea0b7f31149802ed8ee8252378b8a32
eugene <eugene@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-10-01 07:11:06 +0000

    The plugin version has been updated.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/autoblog@10707 f53fe46e-8c02-459b-9fdd-93e75771b278

--- autoblogpremium.php
+++ autoblogpremium.php
@@ -1,30 +1,31 @@
 <?php
 /*
 Plugin Name: AutoBlog
-Version: 3.9.9.7
+Version: 3.9.9.8
 Plugin URI: http://premium.wpmudev.org/project/autoblog
 Description: This plugin automatically posts content from RSS feeds to different blogs on your WordPress Multisite...
-Author: Barry (Incsub)
+Author: Incsub
 Author URI: http://incsub.com
 WDP ID: 97
 */
 
-/*
-Copyright 2007-2012 Incsub (http://incsub.com)
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+// +----------------------------------------------------------------------+
+// | Copyright Incsub (http://incsub.com/)                                |
+// +----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License, version 2, as  |
+// | published by the Free Software Foundation.                           |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to the Free Software          |
+// | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,               |
+// | MA 02110-1301 USA                                                    |
+// +----------------------------------------------------------------------+
 
 // Modify the next line to change the user agent reported to web sites.
 //define('SIMPLEPIE_USERAGENT', 'ABlogPremium/0.1 (Feed Parser; http://premium.wpmudev.org; Allow like Gecko) Build/1');
