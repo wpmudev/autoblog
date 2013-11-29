07a193bbcb238109cd4dc171101085bd9182fd88
eugene <eugene@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-11-29 13:16:33 +0000

    Implemented logs table clean up. Now all logs older than a week will be removed.
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/autoblog@11755 f53fe46e-8c02-459b-9fdd-93e75771b278

--- autoblogincludes/classes/Autoblog/Module/Page/Dashboard.php
+++ autoblogincludes/classes/Autoblog/Module/Page/Dashboard.php
@@ -63,6 +63,9 @@ class Autoblog_Module_Page_Dashboard extends Autoblog_Module {
 			return;
 		}
 
+		// clean up log records older than a week
+		$this->_wpdb->query( sprintf( 'DELETE FROM %s WHERE cron_id < %d', AUTOBLOG_TABLE_FEEDS, strtotime( '-1 week' ) ) );
+
 		// html is not cached, so we need to build it and cache it
 		$template->log_records = $this->_get_log_records();
 
