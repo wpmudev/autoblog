42792b43de0e13531681b0e68de3a3acbea1b16c
Hoang <hoang@Hoangs-MacBook-Pro.local>
2014-07-15 16:18:28 +0700

    Update image caching. Use DomDocument and getimagesize to regconize images instead of using pregex, which is better image result, also can deal with image url don't have extension(google news for example).

--- autoblogincludes/addons/cacheimages.php
+++ autoblogincludes/addons/cacheimages.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 Addon Name: Image Import
 Description: Imports any images in a post to the media library and attaches them to the imported post.
@@ -11,31 +12,47 @@ class A_ImageCacheAddon extends Autoblog_Addon_Image {
 	/**
 	 * Constructor
 	 *
-	 * @since 4.0.0
+	 * @since  4.0.0
 	 *
 	 * @access public
 	 */
 	public function __construct() {
 		parent::__construct();
 
-		$this->_add_action( 'autoblog_post_post_insert', 'import_post_images', 10, 2 );
-		$this->_add_action( 'autoblog_post_post_update', 'import_post_images', 10, 2 );
+		$this->_add_action( 'autoblog_post_post_insert', 'import_post_images', 10, 3 );
+		$this->_add_action( 'autoblog_post_post_update', 'import_post_images', 10, 3 );
 	}
 
 	/**
 	 * Imports post images.
 	 *
-	 * @since 4.0.0
+	 * @since  4.0.0
 	 * @action autoblog_post_post_insert 10 2
 	 * @action autoblog_post_post_update 10 2
 	 *
 	 * @access public
-	 * @param int $post_id The post id.
+	 *
+	 * @param int   $post_id The post id.
 	 * @param array $details The feed settings.
 	 */
-	public function import_post_images( $post_id, $details ) {
-		$post = get_post( $post_id );
-		$images = $this->_get_remote_images_from_content( $post->post_content );
+	public function import_post_images( $post_id, $details, $item ) {
+		$post       = get_post( $post_id );
+		$new_images = $this->_import_post_images( $post->post_content, $details );
+		if ( count( $new_images ) ) {
+			$post->post_content = str_replace( array_keys( $new_images ), array_values( $new_images ), $post->post_content );
+			wp_update_post( $post->to_array() );
+		} else {
+			//something happen, we will need to use the raw content, please note that simplepie will
+			//sanitize content, this is the most case cause google news image don't display
+			$new_images       = $this->_import_post_images( $this->_get_simplepie_item_raw( $item ), $details );
+			$replaced_content = $this->_replace_content_with_new_images( $new_images, $post->post_content );
+			$post->post_content = $replaced_content;
+			wp_update_post( $post->to_array() );
+		}
+	}
+
+	public function _import_post_images( $content, $details ) {
+		$images = $this->_get_remote_images_from_post_content( $content );
 		if ( empty( $images ) ) {
 			return;
 		}
@@ -45,7 +62,7 @@ class A_ImageCacheAddon extends Autoblog_Addon_Image {
 
 		$new_images = array();
 		foreach ( $images as $image ) {
-			if ( !preg_match( '/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $image ) ) {
+			if ( $this->validate_images( $image ) == false ) {
 				continue;
 			}
 
@@ -56,25 +73,81 @@ class A_ImageCacheAddon extends Autoblog_Addon_Image {
 			// Parse the feed url
 			$furl = autoblog_parse_mb_url( $details['url'] );
 
-			if ( empty( $purl['host'] ) && !empty( $furl['host'] ) ) {
+			if ( empty( $purl['host'] ) && ! empty( $furl['host'] ) ) {
 				// We need to add in a host name as the images look like they are relative to the feed
 				$newimage = trailingslashit( $furl['host'] ) . ltrim( $newimage, '/' );
 			}
 
-			if ( empty( $purl['scheme'] ) && !empty( $furl['scheme'] ) ) {
+			if ( empty( $purl['scheme'] ) && ! empty( $furl['scheme'] ) ) {
 				$newimage = substr( $newimage, 0, 2 ) == '//'
 					? $furl['scheme'] . ':' . $newimage
 					: $furl['scheme'] . '://' . $newimage;
 			}
-
 			$newimage_id = $this->_download_image( $newimage, $post_id );
 			if ( $newimage_id ) {
 				$new_images[$image] = wp_get_attachment_url( $newimage_id );
 			}
+
+			return $new_images;
+		}
+	}
+
+	private function _replace_content_with_new_images( $new_images, $content ) {
+		$new_images = array_values( $new_images );
+		//get the image by index
+		$doc         = new DOMDocument();
+		$can_use_dom = @$doc->loadHTML( $content );
+		if ( $can_use_dom ) {
+			$imgs = $doc->getElementsByTagName( 'img' );
+			foreach ( $imgs as $key => $img ) {
+				//replace the old source with new source
+				$new_source = @$new_images[$key];
+				if ( ! empty( $new_source ) ) {
+					$img->setAttribute( 'src', $new_source );
+				}
+			}
+			return $doc->saveHTML();
+		}
+		return $content;
+	}
+
+
+	private function _get_simplepie_item_raw( $item ) {
+		$content_namespaces = array(
+			SIMPLEPIE_NAMESPACE_ATOM_10                => 'content',
+			SIMPLEPIE_NAMESPACE_ATOM_03                => 'content',
+			SIMPLEPIE_NAMESPACE_RSS_10_MODULES_CONTENT => 'content'
+		);
+
+		$summary_namespaces = array(
+			SIMPLEPIE_NAMESPACE_ATOM_10 => 'summary',
+			SIMPLEPIE_NAMESPACE_ATOM_03 => 'summary',
+			SIMPLEPIE_NAMESPACE_RSS_10  => 'description',
+			SIMPLEPIE_NAMESPACE_RSS_20  => 'description',
+			SIMPLEPIE_NAMESPACE_DC_11   => 'description',
+			SIMPLEPIE_NAMESPACE_DC_10   => 'description',
+			SIMPLEPIE_NAMESPACE_ITUNES  => 'summary',
+			SIMPLEPIE_NAMESPACE_ITUNES  => 'subtitle',
+			SIMPLEPIE_NAMESPACE_RSS_090 => 'description',
+		);
+
+		$raw_content = '';
+		foreach ( $content_namespaces as $key => $val ) {
+			$return = $item->get_item_tags( $key, $val );
+			if ( $return ) {
+				$raw_content = $return[0]['data'];
+			}
 		}
 
-		$post->post_content = str_replace( array_keys( $new_images ), array_values( $new_images ), $post->post_content );
-		wp_update_post( $post->to_array() );
+		//if raw content still empty, get from summary
+		foreach ( $summary_namespaces as $key => $val ) {
+			$return = $item->get_item_tags( $key, $val );
+			if ( $return ) {
+				$raw_content = $return[0]['data'];
+			}
+		}
+
+		return $raw_content;
 	}
 
 }
--- autoblogincludes/classes/Autoblog/Addon/Image.php
+++ autoblogincludes/classes/Autoblog/Addon/Image.php
@@ -26,30 +26,32 @@ require_once ABSPATH . 'wp-admin/includes/image.php';
  * Base class for autoblog images related addon.
  *
  * @category Autoblog
- * @package Addon
+ * @package  Addon
  *
- * @since 4.0.0
+ * @since    4.0.0
  */
 class Autoblog_Addon_Image extends Autoblog_Addon {
 
 	/**
 	 * Returns remote images from content.
 	 *
-	 * @since 4.0.0
+	 * @since  4.0.0
 	 *
 	 * @access protected
+	 *
 	 * @param string $content The feed item content.
+	 *
 	 * @return array The array of remote images.
 	 */
 	protected function _get_remote_images_from_content( $content ) {
-		$images = $matches = array();
+		$images  = $matches = array();
 		$siteurl = parse_url( get_option( 'siteurl' ) );
 
 		if ( preg_match_all( '|<img.*?src=[\'"](.*?)[\'"].*?>|is', $content, $matches ) ) {
 			foreach ( $matches[1] as $url ) {
-				$url = str_replace( ' ', '%20', current( explode( '?', $url, 2 ) ) );
+				$url  = str_replace( ' ', '%20', current( explode( '?', $url, 2 ) ) );
 				$purl = autoblog_parse_mb_url( $url );
-				if ( !isset( $purl['host'] ) || $purl['host'] != $siteurl['host'] && preg_match( '/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $url ) ) {
+				if ( ! isset( $purl['host'] ) || $purl['host'] != $siteurl['host'] && preg_match( '/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $url ) ) {
 					// we seem to have an external images
 					$images[] = $url;
 				}
@@ -60,13 +62,69 @@ class Autoblog_Addon_Image extends Autoblog_Addon {
 	}
 
 	/**
+	 * @param $content
+	 *
+	 * @return array
+	 */
+	protected function _get_remote_images_from_post_content( $content ) {
+		$images  = $matches = array();
+		$siteurl = parse_url( get_option( 'siteurl' ) );
+		//look up images by DOM
+		$doc         = new DOMDocument();
+		$can_use_dom = @$doc->loadHTML( $content );
+		if ( $can_use_dom == true ) {
+			$tags = $doc->getElementsByTagName( 'img' );
+			foreach ( $tags as $tag ) {
+				$url = $tag->getAttribute( 'src' );
+				$url = trim( $url );
+				if ( ! empty( $url ) ) {
+					$url = html_entity_decode( rawurldecode( $url ) );
+					//no resouce url please
+					if ( strpos( $url, '//' ) === 0 ) {
+						$url = 'http:' . $url;
+					}
+					$purl = autoblog_parse_mb_url( $url );
+					if ( ! isset( $purl['host'] ) || $purl['host'] != $siteurl['host'] ) {
+						$images[] = $url;
+					}
+				}
+			}
+		} else {
+			//can not parse DOM, use regex
+			$images = $this->_get_remote_images_from_content( $content );
+		}
+
+		return $images;
+	}
+
+	/**
+	 * @param $url
+	 *
+	 * @return bool
+	 */
+	protected function validate_images( $url ) {
+		//validate images
+		$sizes = getimagesize( $url );
+		if ( is_array( $sizes ) ) {
+			$type = current( explode( '/', $sizes['mime'], 2 ) );
+			if ( in_array( $sizes['mime'], get_allowed_mime_types() ) && $type == 'image' ) {
+				return $url;
+			}
+		}
+
+		return false;
+	}
+
+	/**
 	 * Grabs remote image.
 	 *
-	 * @since 4.0.0
+	 * @since  4.0.0
 	 *
 	 * @access protected
-	 * @param string $image The image URL.
-	 * @param int $post_id The post id to attach the image to.
+	 *
+	 * @param string $image   The image URL.
+	 * @param int    $post_id The post id to attach the image to.
+	 *
 	 * @return string|boolean Local image URL on success, otherwise FALSE.
 	 */
 	protected function _download_image( $image, $post_id ) {
@@ -95,11 +153,11 @@ class Autoblog_Addon_Image extends Autoblog_Addon {
 		// Set variables for storage, fix file filename for query strings
 		$matches = array();
 		preg_match( '/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $image, $matches );
-		$file_array['name'] = str_replace( '%20', '-', basename( $matches[0] ) );
+		$file_array['name']     = $this->_generate_image_name($image);
 		$file_array['tmp_name'] = $tmp;
 
 		// do the validation and storage stuff
-		$post = get_post( $post_id );
+		$post     = get_post( $post_id );
 		$image_id = media_handle_sideload( $file_array, $post_id, null, array( 'post_author' => $post->post_author ) );
 		if ( is_wp_error( $image_id ) ) {
 			@unlink( $file_array['tmp_name'] );
@@ -108,6 +166,25 @@ class Autoblog_Addon_Image extends Autoblog_Addon {
 		}
 
 		return $image_id;
+	}
+
+	protected function _generate_image_name( $image ) {
+		$matches   = array();
+		$file_name = '';
+		preg_match( '/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $image, $matches );
+		if ( is_array( $matches ) && count( $matches ) ) {
+			$file_name = str_replace( '%20', '-', basename( $matches[0] ) );
+		} else {
+			$sizes = getimagesize( $image );
+			if ( is_array( $sizes ) && isset( $sizes['mime'] ) && ! empty( $sizes['mime'] ) ) {
+				$ext = image_type_to_extension( $sizes[2] );
+
+				$file_name = substr( sanitize_title( pathinfo( $image, PATHINFO_FILENAME ) ), 0, 254 ) . $ext;
+			}
+		}
+
+		return $file_name;
+
 	}
 
 }
